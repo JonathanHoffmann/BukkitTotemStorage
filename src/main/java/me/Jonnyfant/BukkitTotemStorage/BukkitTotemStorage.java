d318cd80e0d1bbce54b8ccf32dd24a8f3a76ad5a
Jonathan Hoffmann <jonathho@axis.com>
2023-04-05 11:57:44 +0200

    Fix: need Material in inventory even if amount needed is 0; Als: remove unnused imports, reformat, comments
    Change-Id: Ia56a157f0c0f77df9150c329f40f15c0b0c026de

--- src/main/java/me/Jonnyfant/BukkitTotemStorage/BukkitTotemStorage.java
+++ src/main/java/me/Jonnyfant/BukkitTotemStorage/BukkitTotemStorage.java
@@ -1,18 +1,12 @@
 package me.Jonnyfant.BukkitTotemStorage;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
 
 public class BukkitTotemStorage extends JavaPlugin {
     private final String CONFIG_AMOUNT_KEY = "Amount of Totem storage per player";
@@ -47,30 +41,46 @@ public class BukkitTotemStorage extends JavaPlugin {
         saveConfig();
         reloadConfig();
         if (getConfig().getInt(CONFIG_AMOUNT_KEY) > getConfig().getInt(p.getName())) {
-            if (stealStuff(Material.TOTEM, p, 1, false) && stealStuff(Material.DIAMOND, p, getConfig().getInt(CONFIG_DIAMOND_KEY), false)) {
+            if (stealStuff(Material.TOTEM, p, 1, false)
+                    && stealStuff(Material.DIAMOND, p, getConfig().getInt(CONFIG_DIAMOND_KEY), false)) {
                 stealStuff(Material.TOTEM, p, 1, true);
                 stealStuff(Material.DIAMOND, p, getConfig().getInt(CONFIG_DIAMOND_KEY), true);
                 getConfig().set(p.getName(), getConfig().getInt(p.getName()) + 1);
-                p.sendMessage("Adding one Totem of undying to your storage. Balance: " + getConfig().getInt(p.getName()) + " Maximum: " + getConfig().getInt(CONFIG_AMOUNT_KEY));
+                p.sendMessage("Adding one Totem of undying to your storage. Balance: " + getConfig().getInt(p.getName())
+                        + " Maximum: " + getConfig().getInt(CONFIG_AMOUNT_KEY));
                 saveConfig();
                 return true;
             } else {
                 p.sendMessage("You need a Totem of undying and " + getConfig().getInt(CONFIG_DIAMOND_KEY) +
                         " diamond(s) in your inventory to pay for storing a totem. Your current storage contains " +
-                        getConfig().getInt(p.getName()) + " totems, maximum is " + getConfig().getInt(CONFIG_AMOUNT_KEY) + ".");
+                        getConfig().getInt(p.getName()) + " totems, maximum is " + getConfig().getInt(CONFIG_AMOUNT_KEY)
+                        + ".");
                 return false;
             }
         } else {
-            p.sendMessage("You currently stored the maximum amount of Tokens of undying: " + getConfig().getInt(CONFIG_AMOUNT_KEY));
+            p.sendMessage("You currently stored the maximum amount of Tokens of undying: "
+                    + getConfig().getInt(CONFIG_AMOUNT_KEY));
             return true;
         }
     }
 
+    // Checks if a certain amount of a Material exists in a players inventory. if
+    // the boolean steal is true, it will delete the Material from the inventory as
+    // well.
+    // Returns true if it is possible to steal the given Material and amount.
+    // Does not work if the Material is split onto multiple stacks in the inventory.
+    // This could be fixed in the future.
     public boolean stealStuff(Material m, Player p, int amount, boolean steal) {
+        // Don't do anything if trying to steal 0 of a Material, to avoid having to have
+        // the Material in inventory anyway.
+        if (amount <= 0)
+            return true;
         PlayerInventory inv = p.getInventory();
         for (ItemStack is : inv.getContents()) {
             if (is != null) {
                 if (is.getType().equals(m) && is.getAmount() >= amount) {
+                    // Allows method to be used both as a check if it would be possible and to
+                    // actually delete the items.
                     if (steal) {
                         is.setAmount(is.getAmount() - amount);
                     }
